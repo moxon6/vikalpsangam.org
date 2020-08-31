c088d8d9e0426e870b2af257c30003f8019b9611
Martin Moxon <moxon6@googlemail.com>
2020-08-31 20:28:13 +0000

    Add eslint disable to subscribe.js

--- vikalpsangam/js/subscribe.js
+++ vikalpsangam/js/subscribe.js
@@ -1,3 +1,6 @@
+/* eslint-disable */ 
+/* TODO: Fix linting issues */
+
 $(function () {
     var emptyEmailMessage = "Enter valid Email Address!";
     var emailRegex = new RegExp('^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}$', "i");
