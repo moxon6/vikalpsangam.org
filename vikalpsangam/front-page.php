a5e141d2c2fd2fe2c023318bc555a6d6d7b78660
Martin Moxon <moxon6@googlemail.com>
2020-09-01 14:34:48 +0000

    Move components to subdirectory

--- vikalpsangam/front-page.php
+++ vikalpsangam/front-page.php
@@ -37,10 +37,10 @@ get_header();
 <section class="top-carousel-wrapper">
     <div class="row no-right-margin">
         <div class="col-md-8 col-xs-12">
-            <?php get_template_part( 'template-parts/carousel', null, [ "article_content" => $article_content ]); ?>
+            <?php get_template_part( 'template-parts/front-page/carousel', null, [ "article_content" => $article_content ]); ?>
         </div>
         <div class="col-md-4 col-xs-12 featured-list">
-            <?php get_template_part( 'template-parts/highlights', null, [ "article_content" => $article_content ]); ?>
+            <?php get_template_part( 'template-parts/front-page/highlights', null, [ "article_content" => $article_content ]); ?>
         </div>
     </div>
 </section>
