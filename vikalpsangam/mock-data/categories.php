3c7d38f83620e9b2f5f673b91bac03f28d47d888
Martin Moxon <moxon6@googlemail.com>
2020-09-02 14:58:01 +0000

    Extract mock data

---
+++ vikalpsangam/mock-data/categories.php
@@ -0,0 +1,71 @@
+<?php
+
+$categories = [
+    [
+        "thumbnail_image" => "/static/media/uploads/.thumbnails/VS-Favicon-30x30.png",
+        "url" => "/article/category/casestudies",
+        "title" => "CASE STUDIES"
+    ],
+    [
+        "thumbnail_image" => "/static/media/uploads/.thumbnails/alternative_economies_1-30x30.png",
+        "url" => "/article/category/alternative-economies",
+        "title" => "ECONOMICS AND TECHNOLOGIES"
+    ],
+    [
+        "thumbnail_image" => "/static/media/uploads/.thumbnails/energy_1-30x30.png",
+        "url" => "/article/category/energy",
+        "title" => "ENERGY"
+    ],
+    [
+        "thumbnail_image" => "/static/media/uploads/.thumbnails/environment-and-ecology-30x30.png",
+        "url" => "/article/category/environment-ecology",
+        "title" => "ENVIRONMENT AND ECOLOGY"
+    ],
+    [
+        "thumbnail_image" => "/static/media/uploads/.thumbnails/water_and_food-30x30.png",
+        "url" => "/article/category/food-and-water",
+        "title" => "FOOD AND WATER"
+    ],
+    [
+        "thumbnail_image" => "/static/media/uploads/.thumbnails/health_1-30x30.png",
+        "url" => "/article/category/health-hygiene",
+        "title" => "HEALTH AND HYGIENE"
+    ],
+    [
+        "thumbnail_image" => "/static/media/uploads/.thumbnails/knowledge-science-and-technology-30x30.png",
+        "url" => "/article/category/knowledge-media",
+        "title" => "KNOWLEDGE AND MEDIA"
+    ],
+    [
+        "thumbnail_image" => "/static/media/uploads/.thumbnails/education-30x30.png",
+        "url" => "/article/category/learning-and-education",
+        "title" => "LEARNING AND EDUCATION"
+    ],
+    [
+        "thumbnail_image" => "/static/media/uploads/.thumbnails/livelihoods-30x30.png",
+        "url" => "/article/category/livelihoods",
+        "title" => "LIVELIHOODS"
+    ],
+    [
+        "thumbnail_image" => "/static/media/uploads/.thumbnails/alternative-politics-30x30.png",
+        "url" => "/article/category/alternative-politics",
+        "title" => "POLITICS"
+    ],
+    [
+        "thumbnail_image" => "/static/media/uploads/.thumbnails/settlements-30x30.png",
+        "url" => "/article/category/settlements",
+        "title" => "SETTLEMENTS AND TRANSPORT"
+    ],
+    [
+        "thumbnail_image" => "/static/media/uploads/.thumbnails/society_and_culture-30x30.png",
+        "url" => "/article/category/society-culture",
+        "title" => "SOCIETY, CULTURE AND PEACE"
+    ],
+    [
+        "thumbnail_image" => "/static/media/img/default.png",
+        "path" => "/article/category/videos-on-alternatives",
+        "title" => "VIDEOS ON ALTERNATIVE INITIATIVES"
+    ],
+]
+
+?>
