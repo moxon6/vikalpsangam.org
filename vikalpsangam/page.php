ef8500d58a931480740c65d06e8db7a53fb8f6e4
Martin Moxon <moxon6@googlemail.com>
2020-09-05 18:16:44 +0000

    Setup page.php template

--- vikalpsangam/page.php
+++ vikalpsangam/page.php
@@ -1,37 +1,22 @@
 <?php
-/**
- * The template for displaying all pages
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site may use a
- * different template.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package vikalpsangam
- */
-
-get_header();
+    get_header();
 ?>
-
-	<main id="primary" class="site-main">
-
-		<?php
-		while ( have_posts() ) :
-			the_post();
-
-			get_template_part( 'template-parts/content', 'page' );
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
-
-	</main><!-- #main -->
-
+<div id="content">
+    <div class="section generic_section">
+        <div class="container main-body-container">
+            <div class="row">
+                <div class="col-md-9 left-section">
+                    <div class="space-xs"></div>
+                    <div class="space-xs"></div>
+                    <div class="space-xs"></div>
+                    <?php the_content(); ?>
+                </div>
+                <div class="col-sm-12 col-md-3 right-section left-border-separator">
+                    <?php get_template_part('template-parts/common/sidebar') ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 <?php
 get_footer();
