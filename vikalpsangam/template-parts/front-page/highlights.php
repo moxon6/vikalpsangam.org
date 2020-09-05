68c3903bb190775a55387733e8657e9902624e4c
Martin Moxon <moxon6@googlemail.com>
2020-09-05 12:30:08 +0000

    Reindent highlights.php

--- vikalpsangam/template-parts/front-page/highlights.php
+++ vikalpsangam/template-parts/front-page/highlights.php
@@ -4,18 +4,18 @@
 <h2>Highlights</h2>
 <ul class="list-unstyled">
     <?php foreach ($article_content as $key => $article) { ?>
-
         <li class="row">
             <div class="col-xs-4">                
                 <img class="img-responsive" src="<?php echo $article['highlight_image'] ?>">
             </div>
             <div class="col-xs-8">
-                <a href="<?php echo $article['absolute_url']; ?>"><h4 id="featured-article" class="media-heading"><?php echo $article['title']; ?></h4>
-                </a>
-                
+                <a href="<?php echo $article['absolute_url']; ?>">
+                    <h4 id="featured-article" class="media-heading">
+                        <?php echo $article['title']; ?>
+                    </h4>
+                </a>                
                 <!-- TODO: Truncate to 130 -->
                 <p><?php echo $article['description']; ?></p>
-
             </div>
         </li>
     <?php } ?>
