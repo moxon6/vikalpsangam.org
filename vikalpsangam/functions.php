16e8e4599bb49305909c170e80c58a2897539e96
Martin Moxon <moxon6@googlemail.com>
2020-09-06 10:32:12 +0000

    Add extra category field

--- vikalpsangam/functions.php
+++ vikalpsangam/functions.php
@@ -239,3 +239,52 @@ require get_template_directory() . '/inc/template-tags.php';
  * Functions which enhance the theme by hooking into WordPress.
  */
 require get_template_directory() . '/inc/template-functions.php';
+
+
+function add_extra_fields_to_category($taxonomy_name){
+    ?>
+    <div class="form-field">
+        <label for="short-description">Short Description</label>
+        <input type="text" name="short-description" id="short-description"/>
+    </div>
+    <?php
+}
+
+add_action('category_add_form_fields','add_extra_fields_to_category');
+
+
+function save_extra_taxonomy_fields($term_id){
+    $term_item = get_term($term_id,'category');
+    $term_slug = $term_item->slug;
+	$term_category_text = sanitize_text_field($_POST['short-description']);
+	update_option('term_category_text_' . $term_slug, $term_category_text); 
+}
+
+add_action('create_category','save_extra_taxonomy_fields');
+
+function edit_extra_fields_for_category($term){
+
+    //collect the term slug
+    $term_slug = $term->slug;
+
+    //collect our saved term field information
+    $term_category_text = get_option('term_category_text_' . $term_slug); 
+    $term_category_textarea = get_option('term_category_textarea_' . $term_slug); 
+    $term_category_select = get_option('term_category_select_' . $term_slug); 
+    $term_category_radio = get_option('term_category_radio_' . $term_slug); 
+
+    //output our additional fields?>
+    <tr class="form-field">
+        <th valign="top" scope="row">
+            <label for="short-description">Short Description</label>
+        </th>
+        <td>
+            <input type="text" name="short-description" id="short-description" value="<?php echo $term_category_text; ?>"/>
+        </td>
+    </tr>
+    <?php
+}
+
+add_action('category_edit_form_fields','edit_extra_fields_for_category');
+
+add_action('edit_category','save_extra_taxonomy_fields');
