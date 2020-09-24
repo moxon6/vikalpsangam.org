7f3bad21523c424206cf668db3ac59c3cbcfb2e3
Martin Moxon <moxon6@googlemail.com>
2020-09-24 18:43:29 +0000

    Update map page

--- vikalpsangam/page-map.php
+++ vikalpsangam/page-map.php
@@ -9,7 +9,18 @@ get_header();
     <div class="section generic_section">
         <div class="container main-body-container">
             <div class="row">
-                <div class="col-md-9 left-section map-page">
+                <div class="col-md-9 left-section">
+
+                    <div class="space-xs"></div>
+                    <div class="space-xs"></div>
+                    <div class="space-xs"></div>
+
+                    <div id="map-page-writeup">
+                        <h2><?php the_title() ?></h2>
+                        <?php the_content() ?>
+                        
+                    </div>
+
                     <div id="map"> </div>
                 </div>
                 <div class="col-sm-12 col-md-3 right-section left-border-separator">
@@ -22,7 +33,6 @@ get_header();
 
 <style>
     #map {
-        margin-top: 50px;
         height: 800px;
     }
 </style>
