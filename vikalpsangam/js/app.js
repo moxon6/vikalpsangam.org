43876194ebc391b824fc6fa88ebb2802062bcc5c
Martin Moxon <moxon6@googlemail.com>
2020-09-26 22:19:13 +0000

    Move rendermap to app.js

--- vikalpsangam/js/app.js
+++ vikalpsangam/js/app.js
@@ -189,6 +189,38 @@ const CollapseDescription = ( function() {
 	};
 }() );
 
+async function renderMap(id) {
+
+	const mean = (arr) => arr.reduce((x,y) => x + y, 0) / arr.length
+
+	async function fetchCoordinates() {
+		const response = await fetch('/wp-json/vikalpsangam/v1/map');
+		const coordinatesMap = await response.json();
+		return Object.values(coordinatesMap);
+	}
+
+	const INITIAL_ZOOM_LEVEL = 5;
+	const coordinates = await fetchCoordinates();
+
+	const map = L.map(id).setView([
+		mean(coordinates.map(x => x.latitude)),
+		mean(coordinates.map(x => x.longitude))
+	], INITIAL_ZOOM_LEVEL);
+	
+	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+	}).addTo(map);
+
+	var markers = L.markerClusterGroup();
+
+	coordinates.forEach(coordinate => {
+		const marker = L.marker([coordinate.latitude, coordinate.longitude])
+		marker.bindPopup(`<a href="${coordinate.url}">${coordinate.title}</a>`)
+		markers.addLayer(marker);
+	})
+	map.addLayer(markers);
+}
+
 $( document ).ready( function() {
 	$( 'a#feature-article' ).hover( function() {
 		$( this ).find( 'p' ).toggle();
