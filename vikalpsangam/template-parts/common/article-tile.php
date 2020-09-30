c78ccc4bbee03efc5be43a070ceaaee210872cc3
Martin Moxon <moxon6@googlemail.com>
2020-09-30 19:50:41 +0000

    Fix article tile height

--- vikalpsangam/template-parts/common/article-tile.php
+++ vikalpsangam/template-parts/common/article-tile.php
@@ -2,7 +2,7 @@
 <div class="col-xs-6 col-sm-4 category">
     <a class="category-link-wrapper" href="<?php echo get_the_permalink(); ?>">
         <div class="top-sub-section">
-            <h4 style="height: 64px;"><?php the_title() ?></h4>
+            <h4><?php the_title() ?></h4>
         </div>
         <div class="category-image-wrapper">
             <?php the_post_thumbnail("large", ["class" => "img-responsive category-image"]) ?>
