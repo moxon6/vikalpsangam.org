0bbbef1da15d9d7cd12e53974e5b0519e3aad995
Martin Moxon <moxon6@googlemail.com>
2020-09-30 16:26:31 +0100

    Add search page

--- vikalpsangam/functions.php
+++ vikalpsangam/functions.php
@@ -422,3 +422,14 @@ function register_custom_query_vars( $vars ) {
     return $vars;
 }
 add_filter( 'query_vars', 'register_custom_query_vars', 1 );
+
+//Exclude pages from WordPress Search
+if (!is_admin()) {
+	function wpb_search_filter($query) {
+	if ($query->is_search) {
+	$query->set('post_type', 'post');
+	}
+	return $query;
+	}
+	add_filter('pre_get_posts','wpb_search_filter');
+}
