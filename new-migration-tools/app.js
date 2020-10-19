3cf534d85a537dcadf2fb04205f53089fe67a0d8
Martin Moxon <moxon6@googlemail.com>
2020-10-19 22:05:35 +0000

    Setup basic sequelize findOne

--- new-migration-tools/app.js
+++ new-migration-tools/app.js
@@ -1,5 +1,7 @@
 const { Sequelize } = require('sequelize');
 
+import { initModels } from './models/django/init-models';
+
 const sequelize = new Sequelize('postgres://postgres:postgres@postgres:5432/main') // Example for postgres
 
 async function main() {
@@ -7,11 +9,11 @@ async function main() {
         await sequelize.authenticate();
         console.log('Connection has been established successfully.');
 
-        const result = await sequelize
-            .query('select table_schema, table_name from information_schema.tables', {
-                type: sequelize.QueryTypes.SELECT
-            })
-        console.log(result)
+        const djangoModels = initModels(sequelize);       
+
+        const post = await djangoModels.blog_blogpost.findOne();
+
+        console.log(post)
 
         await sequelize.close()
     } catch (error) {
