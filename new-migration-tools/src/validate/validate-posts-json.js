b42792961722a06e6e5ff05a6dc8b8e383060ee9
Martin Moxon <moxon6@googlemail.com>
2020-10-21 13:41:08 +0000

    Move lint ignore

--- new-migration-tools/src/validate/validate-posts-json.js
+++ new-migration-tools/src/validate/validate-posts-json.js
@@ -3,12 +3,11 @@ import posts from '../../posts.json';
 import schema from './schema';
 
 async function main() {
-  // eslint-disable-next-line no-restricted-syntax
-
   posts.forEach((post) => {
     try {
       Joi.assert(post, schema);
     } catch (error) {
+      // eslint-disable-next-line no-console
       console.log(`Invalid post ${post.id} : ${error}`);
     }
   });
