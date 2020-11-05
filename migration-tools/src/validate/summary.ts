96f99752930e0979cad7d993328c6fd7641a07d0
Martin Moxon <moxon6@googlemail.com>
2020-11-05 18:10:34 +0000

    Generate full urls in validate summary

--- migration-tools/src/validate/summary.ts
+++ migration-tools/src/validate/summary.ts
@@ -7,15 +7,18 @@ const posts = JSON.parse(
 console.log("posts.json summary")
 console.log(`Number of posts ${posts.length}`)
 
+const SITE = "https://dev.vikalpsangam.org"
 
 const nonASCIIPosts = posts
     .filter(post => post.slug !== encodeURI(post.slug))
+    .map(post => ({...post, slug: `${SITE}/article/${post.slug}`}))
     .slice(0,10)
 console.log("A subset of posts with non-ascii urls to test:")
 console.table(nonASCIIPosts, ["title", "slug"])
 
 const postsWithComments = posts
     .filter(post => post.comments.length > 0)
+    .map(post => ({...post, slug: `${SITE}/article/${post.slug}`}))
     .slice(0,10)
 console.log("A subset of posts with comments to test:")
 console.table(postsWithComments, ["title", "slug"])
