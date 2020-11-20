0693a5c1800a6ea0f6fd28ef85caf60c1cffd7ff
Martin Moxon <moxon6@googlemail.com>
2020-11-20 22:59:06 +0000

    Fix file path issues

--- vikalpsangam/js/site/index.js
+++ vikalpsangam/js/site/index.js
@@ -9,7 +9,15 @@ import 'leaflet.markercluster/dist/MarkerCluster.Default.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import '../../sass/style.scss';
 
+import iconUrl from 'leaflet/dist/images/marker-icon.png';
+import shadowUrl from 'leaflet/dist/images/marker-shadow.png';
+
 async function renderMap(id) {
+  const customIcon = new L.Icon({
+    iconUrl,
+    shadowUrl,
+  });
+
   const mean = (arr) => arr.reduce((x, y) => x + y, 0) / arr.length;
 
   async function fetchCoordinates() {
@@ -21,7 +29,7 @@ async function renderMap(id) {
   const INITIAL_ZOOM_LEVEL = 5;
   const coordinates = await fetchCoordinates();
 
-  const map = L.map(id, { imagePath: '/wp-content/themes/vikalpsangam/bundle' }).setView([
+  const map = L.map(id).setView([
     mean(coordinates.map((x) => x.latitude)),
     mean(coordinates.map((x) => x.longitude)),
   ], INITIAL_ZOOM_LEVEL);
@@ -33,7 +41,7 @@ async function renderMap(id) {
   const markers = new MarkerCluster.MarkerClusterGroup();
 
   coordinates.forEach((coordinate) => {
-    const marker = L.marker([coordinate.latitude, coordinate.longitude]);
+    const marker = L.marker([coordinate.latitude, coordinate.longitude], { icon: customIcon });
     marker.bindPopup(`<a href="${coordinate.url}">${coordinate.title}</a>`);
     markers.addLayer(marker);
   });
