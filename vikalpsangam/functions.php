3af589fea0863ebf921109f2c964a59cc4dab5b1
Martin Moxon <moxon6@googlemail.com>
2020-11-20 20:07:48 +0000

    Rename jquery dependency

--- vikalpsangam/functions.php
+++ vikalpsangam/functions.php
@@ -214,8 +214,8 @@ function vikalpsangam_scripts() {
 	wp_enqueue_style( 'vikalpsangam-style', get_stylesheet_uri(), array(), vikalpsangam_VERSION );
 	wp_style_add_data( 'vikalpsangam-style', 'rtl', 'replace');
 
-	wp_enqueue_script( 'jq', 'https://code.jquery.com/jquery-1.7.1.js', array(), true );	
-	wp_enqueue_script( 'app', get_template_directory_uri() . '/bundle/index.js', ["wp-polyfill"], true );
+	wp_enqueue_script( 'jquery-1.7', 'https://code.jquery.com/jquery-1.7.1.js', array(), true );	
+	wp_enqueue_script( 'app', get_template_directory_uri() . '/bundle/index.js', ["wp-polyfill", "jquery-1.7"], true );
 	
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
