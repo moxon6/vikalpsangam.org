e37e1b9d2fe0a216d9133d80aafc4ac06c74aada
Martin Moxon <moxon6@googlemail.com>
2020-11-20 23:08:02 +0000

    Restructure files

--- vikalpsangam/src/index.js
+++ vikalpsangam/src/index.js
@@ -7,7 +7,7 @@ import 'leaflet/dist/leaflet.css';
 import 'leaflet.markercluster/dist/MarkerCluster.css';
 import 'leaflet.markercluster/dist/MarkerCluster.Default.css';
 import 'bootstrap/dist/css/bootstrap.css';
-import '../../sass/style.scss';
+import './styles/style.scss';
 
 import iconUrl from 'leaflet/dist/images/marker-icon.png';
 import shadowUrl from 'leaflet/dist/images/marker-shadow.png';
