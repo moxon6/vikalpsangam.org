aec4eada55384bc1ebad23c0fdfb6e232fa890a9
Martin Moxon <moxon6@googlemail.com>
2020-11-22 19:26:32 +0000

    Clean up 404.php

--- vikalpsangam/404.php
+++ vikalpsangam/404.php
@@ -10,51 +10,60 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+<div id="content">
+    <div class="section generic_section">
+        <div class="container main-body-container">
+            <div class="row">
+                <div class="col-md-9 left-section">
+                    <main id="primary" class="site-main">
 
-		<section class="error-404 not-found">
-			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'vikalpsangam' ); ?></h1>
-			</header><!-- .page-header -->
+                        <section class="error-404 not-found">
+                            <header class="page-header">
+                                <h1 class="page-title">
+                                    <?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'vikalpsangam' ); ?></h1>
+                            </header>
 
-			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'vikalpsangam' ); ?></p>
+                            <div class="page-content">
+                                <p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'vikalpsangam' ); ?>
+                                </p>
 
-					<?php
-					get_search_form();
+                                <?php
+									get_search_form();
+									the_widget( 'WP_Widget_Recent_Posts' );	
+								?>
 
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
+                                <div class="widget widget_categories">
+                                    <h2 class="widget-title">
+                                        <?php esc_html_e( 'Most Used Categories', 'vikalpsangam' ); ?></h2>
+                                    <ul>
+                                        <?php
+											wp_list_categories(
+												array(
+													'orderby'    => 'count',
+													'order'      => 'DESC',
+													'show_count' => 1,
+													'title_li'   => '',
+													'number'     => 10,
+												)
+											);
+										?>
+                                    </ul>
+                                </div>
 
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'vikalpsangam' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
-							?>
-						</ul>
-					</div><!-- .widget -->
+                            </div>
+                        </section>
 
-					<?php
-					/* translators: %1$s: smiley */
-					$vikalpsangam_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'vikalpsangam' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$vikalpsangam_archive_content" );
+                    </main>
+                </div>
+                <div class="col-sm-12 col-md-3 right-section left-border-separator">
+                    <?php get_template_part('template-parts/common/sidebar') ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
 
-			</div><!-- .page-content -->
-		</section><!-- .error-404 -->
-
-	</main><!-- #main -->
 
 <?php
 get_footer();
