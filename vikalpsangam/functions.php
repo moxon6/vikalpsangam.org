53a123b53a76c3657ac6487b01499bbc6e769901
Martin Moxon <moxon6@googlemail.com>
2020-11-23 22:48:18 +0000

    Remove custom background support

--- vikalpsangam/functions.php
+++ vikalpsangam/functions.php
@@ -83,18 +83,6 @@ if ( ! function_exists( 'vikalpsangam_setup' ) ) :
 				'script',
 			)
 		);
-
-		// Set up the WordPress core custom background feature.
-		add_theme_support(
-			'custom-background',
-			apply_filters(
-				'vikalpsangam_custom_background_args',
-				array(
-					'default-color' => 'ffffff',
-					'default-image' => '',
-				)
-			)
-		);
 	}
 endif;
 add_action( 'after_setup_theme', 'vikalpsangam_setup' );
