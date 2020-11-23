9c589c22da1f0bfe37664234d24cb4e12639c13f
Martin Moxon <moxon6@googlemail.com>
2020-11-23 22:48:04 +0000

    Remove custom logo support

--- vikalpsangam/functions.php
+++ vikalpsangam/functions.php
@@ -95,21 +95,6 @@ if ( ! function_exists( 'vikalpsangam_setup' ) ) :
 				)
 			)
 		);
-
-		/**
-		 * Add support for core custom logo.
-		 *
-		 * @link https://codex.wordpress.org/Theme_Logo
-		 */
-		add_theme_support(
-			'custom-logo',
-			array(
-				'height'      => 250,
-				'width'       => 250,
-				'flex-width'  => true,
-				'flex-height' => true,
-			)
-		);
 	}
 endif;
 add_action( 'after_setup_theme', 'vikalpsangam_setup' );
