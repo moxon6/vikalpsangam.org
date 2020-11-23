a3efacad2fc0fd2178dc44c9e5d16269441bd4d7
Martin Moxon <moxon6@googlemail.com>
2020-11-23 22:11:25 +0000

    Only enqueue in footer

--- vikalpsangam/inc/dependencies.php
+++ vikalpsangam/inc/dependencies.php
@@ -20,7 +20,7 @@ function vikalpsangam_scripts() {
 	wp_enqueue_script( 'vikalpsangam', get_template_directory_uri() . '/js/site/index.js', ["jquery-1.7", "leaflet/js"], true );
 	
 	if(getenv('ENABLE_BROWSERSYNC')) {
-		wp_enqueue_script( 'browser-sync', "http://localhost:3000/browser-sync/browser-sync-client.js", [], Null );
+		wp_enqueue_script( 'browser-sync', "http://localhost:3000/browser-sync/browser-sync-client.js", [], null , $in_footer = true);
 	}
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
