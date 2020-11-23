01ab74a85953445d976cbb3fd5b348860fff6aad
Martin Moxon <moxon6@googlemail.com>
2020-11-23 22:35:03 +0000

    Extract endpoints.php

--- vikalpsangam/functions.php
+++ vikalpsangam/functions.php
@@ -218,45 +218,7 @@ if (is_admin()) {
 	require get_template_directory() . '/inc/admin/admin.php';
 }
 
-
-// Several expensive calls - cache this method?
-function get_article_coordinates($request) {
-
-    $posts = get_posts([
-		"numberposts" => -1
-	]);
-
-	$coordinates = array_map(function($post) {
-		$latitude = get_post_meta($post->ID, 'latitude', TRUE);
-		$longitude = get_post_meta($post->ID, 'longitude', TRUE);
-		return [
-			"title" => get_the_title( $post->ID ),
-			"url" => get_the_permalink($post->ID),
-			"latitude" => (float)$latitude,
-			"longitude" => (float)$longitude,
-		];
-	}, $posts);
-
-	$coordinates = array_filter($coordinates, function($c) {
-		return $c["latitude"] != 0 && $c["longitude"] != 0;
-	});
-
-    if (empty($coordinates)) {
-    	return new WP_Error( 'empty_category', 'the array is empty', array('status' => 404) );
-    }
-
-    $response = new WP_REST_Response($coordinates);
-    $response->set_status(200);
-    return $response;
-}
-
-add_action('rest_api_init', function () {
-	register_rest_route( 'vikalpsangam/v1', 'map', array(
-		'methods'  => 'GET',
-		'callback' => 'get_article_coordinates'
-	));
-});
-
+require get_template_directory() . '/inc/endpoints.php';
 
 function get_category_image($category) {
 	$category_image = z_taxonomy_image_url($category->term_id);
