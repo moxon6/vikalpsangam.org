f7a3b4513b26a910b95056ee6f8c72e0bfd3ab7c
Martin Moxon <moxon6@googlemail.com>
2020-11-29 15:06:18 +0000

    Add plugin header

--- vikalpsangam-blocks/vikalpsangam-blocks.php
+++ vikalpsangam-blocks/vikalpsangam-blocks.php
@@ -1,3 +1,16 @@
 <?php
+/**
+ * Plugin Name:     Vikalpsangam Blocks
+ * Description:     Blocks for vikalpsangam.org
+ * Version:         0.1.0
+ * Author:          Martin Moxon
+ * License:         GPL-2.0-or-later
+ * License URI:     https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:     vikalpsangam-map
+ *
+ * @package         vikalpsangam-blocks
+ */
 
-require_once './viklpsangam-map/viklpsangam-map.php';
+$dir = dirname( __FILE__ );
+
+require_once "$dir/vikalpsangam-map/vikalpsangam-map.php";
--- vikalpsangam-blocks/vikalpsangam-map/vikalpsangam-map.php
+++ vikalpsangam-blocks/vikalpsangam-map/vikalpsangam-map.php
@@ -1,15 +1,4 @@
 <?php
-/**
- * Plugin Name:     Vikalpsangam Map
- * Description:     Example block written with ESNext standard and JSX support – build step required.
- * Version:         0.1.0
- * Author:          Martin Moxon
- * License:         GPL-2.0-or-later
- * License URI:     https://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain:     vikalpsangam-map
- *
- * @package         create-block
- */
 
 function create_block_vikalpsangam_map_block_init() {
 	$dir = dirname( __FILE__ );
