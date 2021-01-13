cdd013d60e63ef438098b15ae9d37d48740146f5
Martin Moxon <moxon6@googlemail.com>
2021-01-13 01:39:35 +0000

    Fix image sizing on homepage stories-by-category

--- template-parts/front-page/categories.php
+++ template-parts/front-page/categories.php
@@ -3,15 +3,17 @@
 ?>
 
 <style>
-    .category-image {
-        width: 70px;
-        height: 70px;
-    }
+.category-image {
+    width: 70px;
+    height: 70px;
+}
 </style>
 
 <div class="container">
     <div class="row">
-        <div class="col-sm-6 col-xs-12"><h2>STORIES BY CATEGORY</h2></div>
+        <div class="col-sm-6 col-xs-12">
+            <h2>STORIES BY CATEGORY</h2>
+        </div>
         <div class="col-sm-6 hidden-xs"><a class="see-all-stories top" href="/article">SEE ALL CATEGORIES</a></div>
     </div>
     <div class="row no-margin">
@@ -25,47 +27,44 @@
                 $category = $categoryPost["category"];
                 $category_image = get_category_image($category);                 
             ?>
-                <div class="col-xs-12 col-sm-6 col-lg-3 category">
+            <div class="col-xs-12 col-sm-6 col-lg-3 category">
 
-                    <div class="row top-sub-section">
+                <div class="row top-sub-section">
 
-                        <div class="col-xs-3">
-                            <img class="img-responsive category-image" src="<?php echo $category_image ?>" />
-                        </div>
-
-                        <div class="col-xs-9">
-                            <h4>
-                                <a class="see-all-stories" href="/article/category/<?php echo $category->slug; ?>">
-                                    <?php echo $category -> name ?>
-                                </a>
-                            </h4>
-                        </div>
-
+                    <div class="col-xs-3">
+                        <img class="img-responsive category-image" src="<?php echo $category_image ?>" />
                     </div>
 
-                    <div class="row image-sub-section">
-                        <div class="col-xs-12 stories-by-category">
-                            <?php the_post_thumbnail(["auto",221], ["class" => "img-responsive"]); ?>
-                        </div>
-                    </div>
-
-                    <div class="row plain-text-sub-section">
-                        <p class="col-xs-12">
-                            <a class="see-all-stories" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-                        </p>
+                    <div class="col-xs-9">
+                        <h4>
+                            <a class="see-all-stories" href="/article/category/<?php echo $category->slug; ?>">
+                                <?php echo $category -> name ?>
+                            </a>
+                        </h4>
                     </div>
 
                 </div>
 
+                <div class="row image-sub-section">
+                    <div class="col-xs-12 stories-by-category">
+                        <?php the_post_thumbnail("medium", ["class" => "img-responsive"]); ?>
+                    </div>
+                </div>
+
+                <div class="row plain-text-sub-section">
+                    <p class="col-xs-12">
+                        <a class="see-all-stories" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                    </p>
+                </div>
+
+            </div>
+
             <?php } ?>
 
         </div>
-        
+
         <div class="col-xs-12 see-all-stories-wrapper">
-                <a class="see-all-stories visible-xs" href="/article">SEE ALL CATEGORIES</a>
+            <a class="see-all-stories visible-xs" href="/article">SEE ALL CATEGORIES</a>
         </div>
     </div>
 </div>
-
-    
-
