c5c396f9f9281e6fcb1bfd392c7ea89d1175184a
Martin Moxon <moxon6@googlemail.com>
2021-01-25 18:00:16 +0000

    Remove unnecessary templates

--- page.php
+++ page.php
@@ -6,7 +6,7 @@
         <div class="container main-body-container">
             <div class="row">
                 <div class="col-md-9 left-section">
-
+                    <h2><?php the_title() ?></h2>
                     <?php the_content(); ?>
                 </div>
                 <div class="col-sm-12 col-md-3 right-section left-border-separator">
