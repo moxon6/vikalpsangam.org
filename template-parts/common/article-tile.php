0ec9b9967df1f8791b13d84e4a555efdb5cebf3c
Martin Moxon <moxon6@googlemail.com>
2021-01-29 15:57:28 +0000

    Override bootstrap themes

--- template-parts/common/article-tile.php
+++ template-parts/common/article-tile.php
@@ -4,7 +4,10 @@
         <div class="card-body">
             <h6 class="card-title article-tile-title"><?php the_title() ?></h6>
             <p class="card-text article-tile-text"><?php echo filter_excerpt(get_the_excerpt()); ?></p>
-            <a href="<?php echo get_the_permalink(); ?>" class="btn btn-primary">Read More</a>
+
         </div>
+        <footer class="card-footer article-tile-footer">
+            <a href="<?php echo get_the_permalink(); ?>" class="btn btn-primary btn-sm">Read More</a>
+        </footer>
     </div>
 </div>
