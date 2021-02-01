cfa57c822c4c7f23d65ef00db892bf7c26ed70ad
Martin Moxon <moxon6@googlemail.com>
2021-02-01 10:54:19 +0000

    Add links to tiles

--- template-parts/common/article-tile.php
+++ template-parts/common/article-tile.php
@@ -15,12 +15,15 @@
         </a>
 
         <?php endif ?>
+        <a href="<?php echo get_the_permalink(); ?>">
 
-        <?php the_post_thumbnail("large", ["class" => "card-img-top article-tile-image"]) ?>
+            <?php the_post_thumbnail("large", ["class" => "card-img-top article-tile-image"]) ?>
+        </a>
         <div class="card-body">
-            <h6 class="card-title article-tile-title"><?php the_title() ?></h6>
+            <a href="<?php echo get_the_permalink(); ?>">
+                <h6 class="card-title article-tile-title"><?php the_title() ?></h6>
+            </a>
             <p class="card-text article-tile-text"><?php echo filter_excerpt(get_the_excerpt()); ?></p>
-
         </div>
         <footer class="card-footer article-tile-footer">
             <a href="<?php echo get_the_permalink(); ?>" class="btn btn-primary btn-sm">Read More</a>
--- template-parts/common/highlight-card.php
+++ template-parts/common/highlight-card.php
@@ -1,11 +1,15 @@
 <div class="card mb-2 highlight-card container-fluid">
     <div class="row g-0 highlight-card-row justify-content-space-between">
         <div class="col-3 highlight-card-thumbnail-col">
-            <?php the_post_thumbnail('thumbnail', array('class' => 'img-fluid featured-list-image')); ?>
+            <a href="<?php echo get_the_permalink() ?>">
+                <?php the_post_thumbnail('thumbnail', array('class' => 'img-fluid featured-list-image')); ?>
+            </a>
         </div>
         <div class="col-9 highlight-card-text-col">
             <div class="card-body featured-list-card-body">
-                <h6><?php the_title(); ?></h6>
+                <a href="<?php echo get_the_permalink() ?>">
+                    <h6><?php the_title(); ?></h6>
+                </a>
                 <?php the_excerpt(); ?>
             </div>
         </div>
