2981d2f914ce49f63d188400dab43ce509ff443f
Martin Moxon <moxon6@googlemail.com>
2021-02-01 14:53:53 +0000

    Initial work for page-about template

--- functions.php
+++ functions.php
@@ -9,7 +9,35 @@
 
 include 'version.php';
 
-require_once( __DIR__ . '/vendor/autoload.php' );
+$composer_autoload = __DIR__ . '/vendor/autoload.php';
+
+if ( file_exists( $composer_autoload ) ) {
+	require_once $composer_autoload;
+	$timber = new Timber\Timber();
+}
+
+if ( ! class_exists( 'Timber' ) ) {
+
+	add_action(
+		'admin_notices',
+		function() {
+			echo '<div class="error"><p>Timber not activated. Make sure you activate the plugin in <a href="' . esc_url( admin_url( 'plugins.php#timber' ) ) . '">' . esc_url( admin_url( 'plugins.php' ) ) . '</a></p></div>';
+		}
+	);
+
+	add_filter(
+		'template_include',
+		function( $template ) {
+			return get_stylesheet_directory() . '/static/no-timber.html';
+		}
+	);
+	return;
+}
+
+
+Timber::$dirname = array( 'templates', 'views' );
+Timber::$autoescape = false;
+
 
 if ( ! function_exists( 'vikalpsangam_setup' ) ) :
 	/**
