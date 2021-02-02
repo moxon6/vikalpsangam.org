0e64dfad4337b117853a5d2b9ab9d77f178a1a7a
Martin Moxon <moxon6@googlemail.com>
2021-02-02 16:01:15 +0000

    Use page-about as default twig template

--- page.php
+++ page.php
@@ -1,20 +1,6 @@
 <?php
-    get_header();
-?>
-<div id="page">
-    <div class="section generic_section">
-        <div class="container-fluid">
-            <div class="row">
-                <main class="col-xl-8 col-md-12 left-section">
-                    <h2><?php the_title() ?></h2>
-                    <?php the_content(); ?>
-                </main>
-                <aside class="col-xl-4 col-md-12 sidebar-aside">
-                    <?php get_sidebar() ?>
-                </aside>
-            </div>
-        </div>
-    </div>
-</div>
-<?php
-get_footer();
+
+$context = Timber::context();
+$timber_post = new Timber\Post();
+$context['post'] = $timber_post;
+Timber::render( array( 'page-' . $timber_post->post_name . '.twig', 'page.twig' ), $context );
