8341df527b290d39661a1cd280099ebbb50d0d32
Martin Moxon <moxon6@googlemail.com>
2021-02-02 16:35:53 +0000

    Reuse term-header, tag and category pages

--- category.php
+++ category.php
@@ -3,5 +3,6 @@
 $context = Timber::context();
 $timber_post = new Timber\Post();
 $context['post'] = $timber_post;
+$context['term'] = new Timber\Term();
 
-Timber::render( array( 'page-category.twig', 'page.twig' ), $context );
+Timber::render( array( 'page-term.twig', 'page.twig' ), $context );
--- tag.php
+++ tag.php
@@ -1,39 +1,8 @@
 <?php
-    get_header();
-?>
 
-<div id="page">
-    <div class="section generic_section">
-        <div class="container-fluid">
-            <div class="row">
-                <main class="col-md-8 left-section">
+$context = Timber::context();
+$timber_post = new Timber\Post();
+$context['post'] = $timber_post;
+$context['term'] = new Timber\Term();
 
-
-                    <div class="category-page-heading">
-                        <div class="category-title-link no-icon"><?php single_tag_title() ?></div>
-                    </div>
-                    <div id="infinite-scroll-content"
-                        class="row category-page-category-list-wrapper category-topping-wrapper">
-                        <?php
-                            while (have_posts()) {
-                                the_post();
-                                ?>
-                        <div class="col-xs-4 col-md-6 col-lg-4 pb-4">
-                            <?php get_template_part( 'template-parts/common/article-tile'); ?>
-                        </div>
-                        <?php                            }
-                        ?>
-                    </div>
-                </main>
-
-                <aside class="col-sm-12 col-md-4 sidebar-aside">
-                    <?php get_sidebar() ?>
-                </aside>
-            </div>
-        </div>
-    </div>
-</div>
-
-<?php
-
-get_footer();
+Timber::render( array( 'page-term.twig', 'page.twig' ), $context );
