0db2d55d73650f6d6315908adbc82b4655fa465b
Martin Moxon <moxon6@googlemail.com>
2021-02-02 19:14:57 +0000

    Use twig for page-resource.php

--- page-resources.php
+++ page-resources.php
@@ -10,23 +10,9 @@
     }
 ?>
 
-<?php 
-    get_header();
-?>
-<div id="page">
-    <div class="section generic_section">
-        <div class="container-fluid">
-            <div class="row">
-                <main class="col-md-8 left-section">
-                    <h2><?php the_title() ?></h2>
-                    <?php the_content(); ?>
-                </main>
-                <aside class="col-sm-12 col-md-4 sidebar-aside">
-                    <?php get_sidebar() ?>
-                </aside>
-            </div>
-        </div>
-    </div>
-</div>
 <?php
-get_footer();
+
+$context = Timber::context();
+$timber_post = new Timber\Post();
+$context['post'] = $timber_post;
+Timber::render( array( 'page.twig' ), $context );
