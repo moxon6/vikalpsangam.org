3eca2cc1eb5c03bc72d471bf6d7ce838f0ab1a3e
Martin Moxon <moxon6@googlemail.com>
2021-02-16 19:39:33 +0000

    Simplify dependencies

--- inc/dependencies.php
+++ inc/dependencies.php
@@ -10,17 +10,25 @@ function vikalpsangam_scripts() {
 	wp_enqueue_script("bootstrap/js", "https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js");
 
 	wp_enqueue_script("autosize/js", "https://cdn.jsdelivr.net/npm/autosize@4.0.2/dist/autosize.min.js");
-	wp_enqueue_script("diffhtml", "//unpkg.com/diffhtml/dist/diffhtml.min.js");	
+	wp_enqueue_script("diffhtml", "//unpkg.com/diffhtml/dist/diffhtml.min.js");
+	
 
 	wp_enqueue_style("slick/css", "//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css");
 	wp_enqueue_style("slick-theme/css", "//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css");
 	wp_enqueue_script("slick/js", "//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js", [ "jquery" ]);	
-	
+
+    wp_enqueue_script("leaflet/js", "https://unpkg.com/leaflet@1.7.1/dist/leaflet.js");
+	wp_enqueue_style( 'leaflet/css', 'https://unpkg.com/leaflet@1.7.1/dist/leaflet.css' );
+
+	wp_enqueue_script("vue/js", "//cdn.jsdelivr.net/npm/vue@2.6.12/dist/vue.js");
+
+	wp_enqueue_script("vue-leaflet/js", "//unpkg.com/vue2-leaflet", ["vue/js", "leaflet/js"]);
+
 	wp_enqueue_style( 'vikalpsangam-style', get_stylesheet_uri(), array(), vikalpsangam_VERSION );
 	wp_style_add_data( 'vikalpsangam-style-rtl', 'rtl', 'replace');
 
-	wp_enqueue_script( 'vikalpsangam-comments-ajax/js', get_template_directory_uri() . '/js/site/comments-ajax.js', ["jquery", "leaflet/js", "wp-api"], vikalpsangam_VERSION );
-	wp_enqueue_script( 'vikalpsangam-stories-map/js-vue', get_template_directory_uri() . '/js/site/stories-map-vue.js', ["jquery", "wp-api"], random_int(PHP_INT_MIN, PHP_INT_MAX) );
+	wp_enqueue_script( 'vikalpsangam', get_template_directory_uri() . '/js/site/comments-ajax.js', ["jquery", "wp-api"], vikalpsangam_VERSION );
+	wp_enqueue_script( 'vikalpsangam-map', get_template_directory_uri() . '/js/site/stories-map.js', ["vue-leaflet/js"], vikalpsangam_VERSION );
 	
 	wp_enqueue_script( 'wp-api' );
 	
--- js/site/stories-map.js
+++ js/site/stories-map.js
@@ -1,150 +1,68 @@
-const categories = [
-  'ECONOMICS AND TECHNOLOGIES',
-  'ENERGY',
-  'ENVIRONMENT AND ECOLOGY',
-  'FOOD AND WATER',
-  'HEALTH AND HYGIENE',
-  'LEARNING AND EDUCATION',
-  'LIVELIHOODS',
-  'POLITICS',
-  'SETTLEMENTS AND TRANSPORT',
-  'SOCIETY, CULTURE AND PEACE',
-  'OTHERS',
-];
-
-const colors = [
-  '#a6cee3',
-  '#1f78b4',
-  '#b2df8a',
-  '#33a02c',
-  '#fb9a99',
-  '#e31a1c',
-  '#fdbf6f',
-  '#ff7f00',
-  '#cab2d6',
-  '#6a3d9a',
-  '#dddddd',
-];
-
-const colorIcon = (color) => {
-  const styles = `
-    background-color: ${color};
-    width: 100%;
-    height: 100%;
-    border: 1px solid black;
-    border-radius: 50%;
-    display: block;`;
-
-  return L.divIcon({
-
-    html: `<span style="${styles}" />`,
-  });
-};
-
-const icons = colors.map((color) => colorIcon(color));
-
-const randomIcon = () => icons[Math.floor(Math.random() * icons.length)];
-
-
-const INITIAL_ZOOM_LEVEL = 6;
 
 const getBounds = (coordinates) => new L.LatLngBounds([
-  [Math.min(...coordinates.map((x) => x.latitude)), Math.min(...coordinates.map((x) => x.longitude))],
-  [Math.max(...coordinates.map((x) => x.latitude)), Math.max(...coordinates.map((x) => x.longitude))],
+    [Math.min(...coordinates.map((x) => x.latitude)), Math.min(...coordinates.map((x) => x.longitude))],
+    [Math.max(...coordinates.map((x) => x.latitude)), Math.max(...coordinates.map((x) => x.longitude))],
 ]);
 
-function addAttribution(map) {
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-  }).addTo(map);
-}
+async function renderMap() {
 
-function addLegend(map, categories, selectCategory) {
+    Vue.component('l-map', window.Vue2Leaflet.LMap);
+    Vue.component('l-tile-layer', window.Vue2Leaflet.LTileLayer);
+    Vue.component('l-marker', window.Vue2Leaflet.LMarker);
+    Vue.component('l-popup', window.Vue2Leaflet.LPopup);
+    Vue.component('l-control', window.Vue2Leaflet.LControl);
 
-	var legend = L.control({position: 'bottomright'});
+    const responseJson = await wp.apiRequest({ path: `vikalpsangam/v2/map` });
+    
+    const categories = _
+        .chain(responseJson.categories)
+        .map(category => ({
+            ...category,
+            icon: L.divIcon({
+                className: "story-marker-icon",
+                html: `<div style='background-color: ${category.color}'></div>`
+            })
+        }))
+        .indexBy("cat_ID")
+        .value()
+    
+    const coordinates = Object.values(responseJson.coordinates)
+    
+    debugger;
 
-	legend.onAdd = function () {
-    const div = L.DomUtil.create('div')
-    debugger
-    const labels = categories.map((category, i) => `
-    <li data-category=${category.cat_ID} class="category" style="--bullet-color: ${colors[i]}">
-        <div class="bullet"></div>
-        ${category.name}
-    </li>
-    `
-    )
-
-    div.innerHTML = `<ul class="category-list">
-      ${labels.join("")}
-    </ul>`
-
-    div.querySelectorAll(".category").forEach(category => {
-      category.addEventListener("click", (e) => {
-        e.preventDefault()
-        const categoryId = category.dataset.category;
-        console.log("Toggling category " + categoryId)
-      })
-    })
-
-
-		return div;
-	};
-
-	legend.addTo(map);
-}
-
-function filterCategories (activateCategories, categories) {
-  return []
-}
-
-async function renderMap(id) {
-
-  const response = await fetch('/wp-json/vikalpsangam/v2/map');
-  const responseJson = await response.json();
-  const coordinates = Object.values(responseJson.coordinates);
-  const categories = responseJson.categories;
-
-  
-  function selectCategory(selectedCategory) {
-    for (const marker of this.markers) {
-      const { categories } = marker.coordinate
-      if (!selectedCategory || categories.includes(selectedCategory)) {
-        marker.addTo(map)
-      } else {
-        try {
-          marker.remove()  
-        } catch (e) {
+    window.app = new Vue({
+        el: '#large-map',
+        data: {
+            message: 'Hello Vue!',
+            url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+            attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+            zoom: 6,
+            center: getBounds(coordinates).getCenter(),
+            coordinates,
+            categories,
+            selectedCategory: null
+        },
+        computed: {
+            visibleMarkers: function() {
+                return this.selectedCategory
+                    ? this.coordinates.filter(coordinate => coordinate.categories.includes(this.selectedCategory))
+                    : this.coordinates
+            }
+        },
+        methods: {
+            getIcon(coordinate) {
+                return this.selectedCategory 
+                    ? categories[this.selectedCategory].icon
+                    : categories[coordinate.categories[0]].icon
+            },
+            getCategoryStyle(category) {
+                return {
+                    '--bullet-color': category.color
+                }
+            },
+            selectCategory(category) {
+                this.selectedCategory = this.selectedCategory !== category.cat_ID ? category.cat_ID : null;
+            }
         }
-      }
-    }
-  }
-  
-  
-
-  const map = L.map(id, {
-    center: getBounds(coordinates).getCenter(),
-    zoom: INITIAL_ZOOM_LEVEL,
-  });
-
-  addAttribution(map);
-  addLegend(map, categories, selectCategory);
-
-  const markers = coordinates.map(
-    (coordinate) => {
-      const icon = randomIcon()
-      const marker = L.marker([coordinate.latitude, coordinate.longitude], { icon });
-      marker.coordinate = coordinate
-      marker.bindPopup(`<a href="${coordinate.url}">${coordinate.title}</a>`);
-      return marker;
-    },
-  );
-
-  for (const marker of markers) {
-    marker.addTo(map);
-  }
-
-  window.themap = map;
-  window.markers = markers
+    })
 }
-
-window.renderMap = renderMap;
