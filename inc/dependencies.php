b36796f306de376753edfad9ad5057ea8699a3ef
Martin Moxon <moxon6@googlemail.com>
2021-03-20 02:39:38 +0000

    Remove features/stories-map

--- inc/dependencies.php
+++ inc/dependencies.php
@@ -20,10 +20,7 @@ function vikalpsangam_scripts() {
 	wp_style_add_data( 'vikalpsangam-style-rtl', 'rtl', 'replace');
 
 	wp_enqueue_script( 'vikalpsangam/comments-ajax', get_template_directory_uri() . '/js/site/comments-ajax.js', ["jquery", "wp-api"], vikalpsangam_VERSION );
-	
-	wp_enqueue_script( 'vikalpsangam/stories-map', get_template_directory_uri() . '/features/stories-map/dist/index.js', [], vikalpsangam_VERSION );
-	wp_enqueue_style("vikalpsangam/stories-map/css", get_template_directory_uri() . '/features/stories-map/dist/index.css');
-
+		
 	wp_enqueue_script( 'wp-api' );
 	
 	wp_enqueue_script( 'comment-reply' );
