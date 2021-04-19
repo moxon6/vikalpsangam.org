c22aa654b62f3c3c831adc52e52244d5d98a95db
Martin Moxon <moxon6@googlemail.com>
2021-04-20 00:03:56 +0100

    Fix admin.js 404

--- inc/admin/admin.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function admin_overrides(){ 
-    wp_register_script('admin-overrides', get_template_directory_uri() .'/bundle/admin.js', ["wp-polyfill"], vikalpsangam_VERSION, true);
-	wp_enqueue_script('admin-overrides');
-	wp_enqueue_style('reorder-post-sidebar', get_template_directory_uri() . '/admin-sidebar.css');
-}
-add_action( 'admin_enqueue_scripts', 'admin_overrides' );
