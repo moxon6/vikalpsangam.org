ff2e3694936047f4aba0b87e5bd61a00447c0042
Martin Moxon <moxon6@googlemail.com>
2021-06-04 16:45:27 +0100

    Fix 404 page

--- 404.php
+++ 404.php
@@ -7,7 +7,7 @@ $context["recent_posts"] = buffer(function() {
     return the_widget( 'WP_Widget_Recent_Posts' );
 });
 $context["categories"] = buffer(function() {
-    return the_widget( 'WP_Widget_Categories', 'title=CATEGORIES&count=1' )
+    return the_widget( 'WP_Widget_Categories', 'title=CATEGORIES&count=1' );
 });
 
 Timber::render( array( 'pages/_page-not-found.twig' ), $context );
