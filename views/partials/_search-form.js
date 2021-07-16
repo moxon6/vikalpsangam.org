aa417a59e2e7f5745dc49b27d1710813a403d8c8
Martin Moxon <moxon6@googlemail.com>
2021-07-17 00:08:36 +0100

    Update search-form logic

--- views/partials/_search-form.js
+++ views/partials/_search-form.js
@@ -2,6 +2,9 @@ const navBar = document.querySelector('#mega-menu-wrap-header-menu');
 const input = document
   .querySelector('.search-box-input');
 
+const button = document
+  .querySelector('search-box-submit-button');
+
 document
   .querySelector('.search-box-submit-button')
   .addEventListener('click', (e) => {
@@ -12,7 +15,14 @@ document
     }
   });
 
-input
-  .addEventListener('blur', () => {
-    navBar.removeAttribute('menu-open');
-  });
+const form = document.querySelector('.search-box');
+
+button.addEventListener('click', () => {
+  if (navBar.hasAttribute('menu-open')) {
+    form.submit();
+  }
+});
+
+input.addEventListener('blur', () => {
+  navBar.removeAttribute('menu-open');
+});
