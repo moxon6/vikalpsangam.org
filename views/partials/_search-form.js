ffbea85bd82384f61b4d559f0c4b0a81edc1ff11
Martin Moxon <moxon6@googlemail.com>
2021-08-15 20:05:55 +0100

    Simplify search-form

--- views/partials/_search-form.js
+++ views/partials/_search-form.js
@@ -1,9 +1,9 @@
 const navBar = document.querySelector('#mega-menu-wrap-header-menu');
 const input = document
-  .querySelector('.search-box-input');
+  .querySelector('.search-box__input');
 
 const submitButton = document
-  .querySelector('.search-box-submit-button');
+  .querySelector('.search-box__submit-button');
 
 submitButton
   .addEventListener('click', (e) => {
@@ -16,12 +16,5 @@ submitButton
     if (input.value.length === 0) {
       e.preventDefault();
       input.focus();
-    }
-  });
-
-input
-  .addEventListener('blur', (e) => {
-    if (e.relatedTarget !== submitButton) {
-      navBar.removeAttribute('menu-open');
     }
   });
