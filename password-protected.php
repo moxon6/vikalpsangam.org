fd94b72779eff54ebcf01ccb6981162d3758fecb
Martin Moxon <moxon6@googlemail.com>
2021-08-17 02:33:43 +0100

    password-protected -> page-password-protected

--- password-protected.php
+++ password-protected.php
@@ -4,4 +4,4 @@ $context                  = Timber::context();
 $context['post']          = new Timber\Post();
 $context['password_form'] = get_the_password_form();
 
-Timber::render( 'views/pages/_password-protected.twig', $context );
+Timber::render( 'views/pages/_page-password-protected.twig', $context );
