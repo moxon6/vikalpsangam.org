13cdaa1eff5e2b944500594f48aaa87c8907c2d8
Martin Moxon <moxon6@googlemail.com>
2021-08-18 14:34:41 +0100

    Rename front-page context variables

--- front-page.php
+++ front-page.php
@@ -1,25 +1,22 @@
 <?php
+    $context = Timber::context();
+    $context['post'] = new Timber\Post();
 
-$context = Timber::context();
-$timber_post = new Timber\Post();
-$context['post'] = $timber_post;
+    $context["latest_stories"] = Timber::get_posts(array(
+        'numberposts'	=> 8,
+        'post_type'		=> 'post',
+        'category__not_in'	=> $excluded_categories,
+        'has_password'   => false
+    ));
 
-$context["promoted_articles"] = Timber::get_posts(array(
-    'numberposts'	=> 4,
-    'post_type'		=> 'post',
-    'meta_key'		=> 'promoted',
-    'meta_value'	=> '1',
-	'category__not_in'	=> $excluded_categories,
-	'has_password'   => false
-));
+    $context["highlights"] = Timber::get_posts(array(
+        'numberposts'	=> 4,
+        'post_type'		=> 'post',
+        'meta_key'		=> 'promoted',
+        'meta_value'	=> '1',
+        'category__not_in'	=> $excluded_categories,
+        'has_password'   => false
+    ));
 
-$context["latest_posts"] = Timber::get_posts(array(
-    'numberposts'	=> 8,
-    'post_type'		=> 'post',
-	'category__not_in'	=> $excluded_categories,
-	'has_password'   => false
-));
-
-Timber::render( array( 'pages/_page-front.twig' ), $context );
-
+    Timber::render( array( 'pages/_page-front.twig' ), $context );
 ?>
