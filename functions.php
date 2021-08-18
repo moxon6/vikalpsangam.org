df698a79ebc1813597be3df346fb6146632398be
Martin Moxon <moxon6@googlemail.com>
2021-08-18 14:49:25 +0100

    A;ways require autoload

--- functions.php
+++ functions.php
@@ -8,15 +8,9 @@
  */
 
 include "version.php";
+require_once __DIR__ . "/vendor/autoload.php";
 
-$composer_autoload = __DIR__ . "/vendor/autoload.php";
-
-if (file_exists($composer_autoload)) {
-  require_once $composer_autoload;
-  $timber = new Timber\Timber();
-}
-
-Timber::$autoescape = false;
+$timber = new Timber\Timber();
 
 function buffer($fn)
 {
