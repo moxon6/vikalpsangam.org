e5ebbbfebf8cc4db3dda09a3d0d3856004eecb59
Martin Moxon <moxon6@googlemail.com>
2021-08-19 10:38:36 +0100

    Simplify front page design

--- front-page.php
+++ front-page.php
@@ -1,22 +1,28 @@
 <?php
 $context = Timber::context();
-$context["post"] = new Timber\Post();
 
-$context["latest_stories"] = Timber::get_posts([
-  "numberposts" => 8,
-  "post_type" => "post",
-  "category__not_in" => $excluded_categories,
-  "has_password" => false,
-]);
-
-$context["highlights"] = Timber::get_posts([
-  "numberposts" => 4,
-  "post_type" => "post",
-  "meta_key" => "promoted",
-  "meta_value" => "1",
-  "category__not_in" => $excluded_categories,
-  "has_password" => false,
-]);
+$context["front_page_sections"] = [
+  [
+    "header" => "Latest Stories",
+    "posts" => Timber::get_posts([
+      "numberposts" => 8,
+      "post_type" => "post",
+      "category__not_in" => $excluded_categories,
+      "has_password" => false,
+    ]),
+  ],
+  [
+    "header" => "Highlights",
+    "posts" => Timber::get_posts([
+      "numberposts" => 4,
+      "post_type" => "post",
+      "meta_key" => "promoted",
+      "meta_value" => "1",
+      "category__not_in" => $excluded_categories,
+      "has_password" => false,
+    ]),
+  ],
+];
 
 Timber::render(["pages/_page-front.twig"], $context);
 ?>
